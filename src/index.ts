1374ef8be9a1fadb50fbde772bc26acfa9e7c064
Robert Healy <rhealy@coursera.org>
2020-11-17 19:41:06 -0500

    Fix lint errors

--- src/index.ts
+++ src/index.ts
@@ -1,118 +1,119 @@
 import { getInput } from '@actions/core';
-import { Octokit } from "@octokit/rest";
+import { Octokit } from '@octokit/rest';
 import { generateCommand } from 'codeowners-generator';
+import simpleGit, { SimpleGit } from 'simple-git';
+const git: SimpleGit = simpleGit();
 
-type statusType =
-| "in_progress" | "completed";
-type conclusionType =
-| "success" | "failure";
+type statusType = 'in_progress' | 'completed';
+type conclusionType = 'success' | 'failure';
 
 const start = async () => {
-    console.log("Creating check run...");
-    var context = JSON.parse(process.env.GITHUB_CONTEXT || "{}");
-    const GITHUB_TOKEN = getInput('githubToken');
+  console.log('Creating check run...');
+  const context = JSON.parse(process.env.GITHUB_CONTEXT || '{}');
+  const GITHUB_TOKEN = getInput('githubToken');
 
-    const octokit = new Octokit({
-        auth: GITHUB_TOKEN
-    });
+  const octokit = new Octokit({
+    auth: GITHUB_TOKEN,
+  });
 
-    const status : statusType = "in_progress"
+  const status: statusType = 'in_progress';
 
-    var payload = {
-        "name": "CODEOWNERS Check",
-        "owner": context.repository_owner,
-        "repo": context.event.repository.name,
-        "head_sha": context.sha,
-        "status": status,
-        "output": {
-            "title": "Checking CODEOWNERS",
-            "summary": "This check ensures the root CODEOWNERS file was updated to reflect any nested CODEOWNERS files."
-        }
-    };
+  const payload = {
+    name: 'CODEOWNERS Check',
+    owner: context.repository_owner,
+    repo: context.event.repository.name,
+    head_sha: context.sha,
+    status: status,
+    output: {
+      title: 'Checking CODEOWNERS',
+      summary: 'This check ensures the root CODEOWNERS file was updated to reflect any nested CODEOWNERS files.',
+    },
+  };
 
-    console.log("Payload: " + JSON.stringify(payload));
+  console.log('Payload: ' + JSON.stringify(payload));
 
-    await octokit.checks.create(payload);
+  await octokit.checks.create(payload);
 };
 
 const finish = async (conclusion: conclusionType) => {
-    console.log("Marking check run successful...");
-    var context = JSON.parse(process.env.GITHUB_CONTEXT || "{}");
-    const GITHUB_TOKEN = getInput('githubToken');
+  console.log('Marking check run successful...');
+  const context = JSON.parse(process.env.GITHUB_CONTEXT || '{}');
+  const GITHUB_TOKEN = getInput('githubToken');
 
-    const octokit = new Octokit({
-        auth: GITHUB_TOKEN
-    });
+  const octokit = new Octokit({
+    auth: GITHUB_TOKEN,
+  });
 
-    const status : statusType = "completed"
+  const status: statusType = 'completed';
 
-    var payload;
-    switch(conclusion) {
-        case "success":
-            payload = {
-                "owner": context.repository_owner,
-                "repo": context.event.repository.name,
-                "check_run_id": context.event.check_run.id,
-                "status": status,
-                "output": {
-                    "title": "CODEOWNERS Correct!",
-                    "summary": "This check ensures the root CODEOWNERS file was updated to reflect any nested CODEOWNERS files."
-                },
-                "conclusion": conclusion
-            };
-            break;
-        case "failure":
-            payload = {
-                "owner": context.repository_owner,
-                "repo": context.event.repository.name,
-                "check_run_id": context.event.check_run.id,
-                "status": status,
-                "output": {
-                    "title": "Missing CODEOWNERS Changes",
-                    "summary": "Looks like the root CODEOWNERS file has not been updated to reflect nested CODEOWNERS changes. Please run `codeowners-generator generate` and commit to fix."
-                },
-                "conclusion": conclusion
-            };
-            break;
-    }
+  let payload;
+  switch (conclusion) {
+    case 'success':
+      payload = {
+        owner: context.repository_owner,
+        repo: context.event.repository.name,
+        check_run_id: context.event.check_run.id,
+        status: status,
+        output: {
+          title: 'CODEOWNERS Correct!',
+          summary: 'This check ensures the root CODEOWNERS file was updated to reflect any nested CODEOWNERS files.',
+        },
+        conclusion: conclusion,
+      };
+      break;
+    case 'failure':
+      payload = {
+        owner: context.repository_owner,
+        repo: context.event.repository.name,
+        check_run_id: context.event.check_run.id,
+        status: status,
+        output: {
+          title: 'Missing CODEOWNERS Changes',
+          summary:
+            'Looks like the root CODEOWNERS file has not been updated to reflect nested CODEOWNERS changes. Please run `codeowners-generator generate` and commit to fix.',
+        },
+        conclusion: conclusion,
+      };
+      break;
+  }
 
-    console.log("Payload: " + JSON.stringify(payload));
+  console.log('Payload: ' + JSON.stringify(payload));
 
-    await octokit.checks.update(payload);
+  await octokit.checks.update(payload);
 };
 
 const checkCodeOwners = async () => {
-    console.log("Checking codeowners...");
-    try {
-        // Check if CODEOWNERS file is correct by running codeowners-generator and ensuring no changes
-        await generateCommand({parent:{}});
+  console.log('Checking codeowners...');
+  try {
+    // Check if CODEOWNERS file is correct by running codeowners-generator and ensuring no changes
+    await generateCommand({ parent: {} });
 
-        console.log("Called codeowners - check if any change");
+    console.log('Called codeowners - check if any change');
 
-        // const result = await git.status();
+    const result = await git.status();
 
-        // if(result.isClean()) {
-        //     console.log("CODEOWNERS ok!");
-        //     await finish("success");
-        // } else {
-        //     console.log("Need to fix codeowners");
-        //     await finish("failure");
-        // }
-    } catch(e) {
-        console.error(e);
+    if (result.isClean()) {
+      console.log('CODEOWNERS ok!');
+      await finish('success');
+    } else {
+      console.log('Need to fix codeowners');
+      await finish('failure');
     }
+  } catch (e) {
+    console.error(e);
+  }
 };
 
-export const main = async () => {
-    const action = getInput('action');
-    switch(action) {
-        case "START":
-            await start();
-            break;
-        case "CHECK_CODEOWNERS":
-            await checkCodeOwners();
-            break;
-        default:
-            throw new Error("Invalid action - " + action);
-    }
+export const main: () => Promise<void> = async () => {
+  const action = getInput('action');
+  switch (action) {
+    case 'START':
+      await start();
+      break;
+    case 'CHECK_CODEOWNERS':
+      await checkCodeOwners();
+      break;
+    default:
+      throw new Error('Invalid action - ' + action);
+  }
 };
