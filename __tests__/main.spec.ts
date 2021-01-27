36bdc7f8fc4291c5435ebf84516abfddecbf6d1b
Robert Healy <rhealy@coursera.org>
2021-01-27 18:20:44 -0500

    Address comments

--- __tests__/main.spec.ts
+++ __tests__/main.spec.ts
@@ -2,6 +2,6 @@ import { main } from '../src/index';
 
 describe('Main', () => {
   it('should do something', async () => {
-    await expect(main()).rejects.toThrowError;
+    await expect(main()).rejects.toThrowError();
   });
 });
